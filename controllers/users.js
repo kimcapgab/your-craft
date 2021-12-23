173ef531f0b251f7b98f7d23ced43a8a1bbc0df4
Joseph Miller <josephmiller@Josephs-MBP.attlocal.net>
2021-12-23 09:46:22 -0500

    added sign-in controller

--- controllers/users.js
+++ controllers/users.js
@@ -44,3 +44,29 @@ export const signUp = async (req, res) => {
     res.status(400).json({error: error.message})
   }
 }
+
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    
+    const user = await User.findOne({ email: email }).select(
+      'username email password_digest'
+    )
+    if (await bcrypt.compare(password, user.password_digest)) {
+      const payload = {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        exp: parseInt(exp.getTime() / 1000)
+      }
+      const token = jwt.sign(payload, TOKEN_KEY)
+      res.status(201).json({token})
+    } else {
+      res.status(401).send("Invalid Credentials")
+    }
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({error: error.message})
+  }
+}
+
