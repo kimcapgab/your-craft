0be126c383c0b25eb762f062804448a1ddf3f432
Mike2217 <msoles2217@gmail.com>
2022-01-03 11:32:41 -0500

    nav removed from layout

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,12 +1,10 @@
 import './Layout.css'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
-import Nav from '../Nav/Nav'
 
 const Layout = (props) => (
     <div className='layout'>
     <Header user={props.user} />
-    <Nav user={props.user}/>
         <div className="layout-children">
             {props.children}
         </div>
