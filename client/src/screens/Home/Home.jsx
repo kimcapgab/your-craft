425adedb51bce1b86df9311055edbf3408254a11
Joseph Miller <josephmiller@Josephs-MBP.attlocal.net>
2022-01-03 14:58:43 -0500

    sign-up in progress

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,6 +1,8 @@
 import Carosel from "../../components/Carousel/Carousel"
 import { useState, useEffect } from "react"
 import { getBevs } from '../../services/bevApi'
+import Layout from '../../components/Layout/Layout'
+
 
 export default function Home() {
   const [bevs, setBevs] = useState([])
@@ -16,8 +18,10 @@ export default function Home() {
 
 
   return (
+    <Layout>
     <div>
       <Carosel bevs={bevs}/>
-    </div>
+      </div>
+      </Layout>
   )
 }
--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -1,27 +1,58 @@
 import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react'
+import { signUp } from '../../services/userApi'
+import {useNavigate} from 'react-router-dom'
 
-export default function SignUp() {
+export default function SignUp(props) {
 
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
+  const [isError, setIsError] = useState(false)
+  const [errorMsg, setErrorMsg] = useState('')
+  const navigate = useNavigate()
 
-
-
+  const onSignUp = async (event) => {
+    event.preventDefault()
+    const { setUser } = props
+    const newUser = {
+      username,
+      email,
+      password,
+    }
+    try {
+      const user = await signUp(newUser)
+      setUser(user)
+      navigate('/')
+    } catch (error) {
+      setIsError(true)
+      setErrorMsg('Sign Up Details Invalid')
+      console.log(error)
+    }
+  }
+  const renderError = () => {
+    const toggleForm = isError ? "Error" : ""
+    if (isError) {
+      return (
+        <button type="submit" className={toggleForm}>{errorMsg}</button>
+      )
+    } else {
+      return <button type="submit">Sign Up</button>
+    }
+  }
 
   return (
     <div>
       <div>
         <h2>Sign Up Here!</h2>
       </div>
-      <form>
+      <form onSubmit={onSignUp}>
         <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
         <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-        <button type="button">Sign Up!</button>
+      {renderError()}
       </form>
     </div>
   )
