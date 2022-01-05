0e684f7ec6adc8e1bf8c8b343e80bdaf4904d804
Rgarf <nothanks@gmail.com>
2022-01-05 15:34:15 -0500

    fixed HomeApiLinks

--- client/src/components/HomeApiLinks/HomeApiLinks.jsx
+++ client/src/components/HomeApiLinks/HomeApiLinks.jsx
@@ -21,19 +21,19 @@ function HomeApiLinks({ user, bevs, setUser}) {
       {
         user ?
           <div>
-            <h1>{`Welcome, ${user.username}`}</h1>
-            <button onClick={()=>{nav('/create')}}>Create A Beverage</button>
-            <button onClick={handleSignOut}>Sign Out</button>
+            <h1 >{`Welcome, ${user.username}`}</h1>
+            <button id='create-a-bev' onClick={()=>{nav('/create')}}>Create A Beverage</button>
+            <button id='sign-outRon' onClick={handleSignOut}>Sign Out</button>
             <Carosel bevs={bevs}/>
           </div>
           :
           <div>
             <Carosel bevs={bevs}/>
-            <h1>Brewers, Distillers, and Winemakers</h1>
+            <h1 >Brewers, Distillers, and Winemakers</h1>
             <h2>Create an account with us today to promote <br />  YourCraft!</h2>
               <div className='center-buttons-7'>
-                <button onClick={() => nav('/sign-in')}>Sign In</button>
-                <button onClick={() => nav('/sign-up')}>Sign Up</button>
+                <button id='sign-inRon' onClick={() => nav('/sign-in')}>Sign In</button>
+                <button id='sign-upRon' onClick={() => nav('/sign-up')}>Sign Up</button>
               </div>
           </div>
       }
