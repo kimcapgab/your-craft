30e7a0224b80f1c89172d0e536972efa2639b1d6
Mike2217 <msoles2217@gmail.com>
2022-01-06 10:58:35 -0500

    create form styling

--- client/src/screens/CreateForm/CreateForm.jsx
+++ client/src/screens/CreateForm/CreateForm.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useState } from "react";
 import { createBev } from "../../services/bevApi";
 import { useNavigate } from "react-router-dom";
-import './CreateForm.css'
+import "./CreateForm.css";
+
 
 export default function CreateForm({ setToggle }) {
   const [bev, setBev] = useState({
@@ -35,15 +36,19 @@ export default function CreateForm({ setToggle }) {
   };
 
   return (
-      <form id='create-form' onSubmit={handleSubmit}>
-      <div id='left-container-create-form'>
+    <form id="create-form" onSubmit={handleSubmit}>
+      <div id="left-container-create-form">
+        { bev.imgURL !== '' && 
+          <img src={bev.imgURL} id='bev-image'alt='preview of image' />}
+        <div id='image-preview'>
+          <p>URL for Image of Beverage (Transparent Backgrounds Work Best)  </p>
         <input
           className="left-container-item"
-          placeholder="URL for Image (preferrably with a transparent background)"
           value={bev.imgURL}
           name="imgURL"
           onChange={handleChange}
-        />
+          />
+          </div>
         <label for="type-selector">Type: </label>
         <select
           className="left-container-item"
@@ -56,61 +61,84 @@ export default function CreateForm({ setToggle }) {
           <option value="beer">Beer</option>
           <option value="wine">Wine</option>
           <option value="liquor">Liquor</option>
-          </select>
-          </div>
-        <div id='bev-card-container'>
-        <input
-          className="bev-card-item"
-          placeholder="Title"
-          value={bev.title}
-          name="title"
-          onChange={handleChange}
-        />
-        <input
-          className="bev-card-item"
-          placeholder="Style"
-          value={bev.style}
-          name="style"
-          onChange={handleChange}
-        />
-        <input
-          className="bev-card-item"
-          placeholder="Description"
-          value={bev.description}
-          name="description"
-          onChange={handleChange}
-        />
-        <input
-          className="bev-card-item"
-          placeholder="Aroma"
-          value={bev.aroma}
-          name="aroma"
-          onChange={handleChange}
-        />
-        <input
-          className="bev-card-item"
-          placeholder="Taste"
-          value={bev.taste}
-          name="taste"
-          onChange={handleChange}
-        />
-        <input
-          className="bev-card-item"
-          type="number"
-          placeholder="ABV"
-          value={bev.abv}
-          name="abv"
-          onChange={handleChange}
-        />
-        <input
-          className="bev-card-item"
-          placeholder="Website"
-          value={bev.website}
-          name="website"
-          onChange={handleChange}
-        />
+        </select>
+      </div>
+      <div id="bev-card-container">
+        <div id="top-bev-card">
+          <input
+            className="bev-card-item space-me"
+            id="right-title"
+            placeholder="Title:"
+            value={bev.title}
+            name="title"
+            onChange={handleChange}
+          />
+          <input
+            className="bev-card-item space-me"
+            id="right-style"
+            placeholder="Style:"
+            value={bev.style}
+            name="style"
+            onChange={handleChange}
+          />
+          <input
+            className="bev-card-item space-me"
+            id="description"
+            type='textarea'
+            placeholder="Description:"
+            value={bev.description}
+            name="description"
+            onChange={handleChange}
+          />
+          <h2>Characteristics</h2>
         </div>
+        <div id="bottom-bev-card">
+          <div className='bottom-right-input'>
+            <p>Aroma:</p>
+            <input
+              className="bev-card-item"
+              id="right-aroma"
+              value={bev.aroma}
+              name="aroma"
+              onChange={handleChange}
+            />
+          </div>
+          <div className='bottom-right-input'>
+            <p>Taste:</p>
+            <input
+              className="bev-card-item"
+              id="right-taste"
+              value={bev.taste}
+              name="taste"
+              onChange={handleChange}
+            />
+          </div>
+          <div className='bottom-right-input'>
+            <p>ABV:</p>
+            <input
+              className="bev-card-item"
+              id="right-abv"
+              type="number"
+              value={bev.abv}
+              name="abv"
+              onChange={handleChange}
+            />
+          </div>
+          <div className='bottom-right-input'>
+            <p>Link to Buy:</p>
+            <input
+              className="bev-card-item"
+              id="right-website"
+              value={bev.website}
+              name="website"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+      </div>
+      <div id="bev-card-submit">
         <button onClick={handleSubmit}>Submit</button>
-      </form>
+      </div>
+    </form>
   );
 }
