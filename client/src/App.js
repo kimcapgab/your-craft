29dd4225471e406f1900d8a3c957be8d826c0680
Rgarf <nothanks@gmail.com>
2022-01-07 13:32:29 -0500

    app

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import BevGallery from './screens/BevGallery/BevGallery'
 import BevID from './screens/BevID/BevID'
 import CreateForm from './screens/CreateForm/CreateForm'
 import { useEffect, useState } from 'react';
-import {verifyUser} from './services/userApi'
+import {verifyUser, getUsers} from './services/userApi'
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout/Layout'
 import About from './screens/About/About';
@@ -21,6 +21,7 @@ function App() {
   const [user, setUser] = useState(null)
   const [toggle, setToggle] = useState(false)
 
+
   useEffect(() => {
     const fetchUser = async () => {
       const user = await verifyUser()
@@ -28,22 +29,24 @@ function App() {
     }
     fetchUser()
   }, [])
-  
+
+
 
   return (
     <div className="App">
       <Layout user={user} setUser={setUser} >
       <Routes>
         <Route path='/' element={<Home user={user} setUser={setUser} toggle={toggle} setToggle={setToggle}/>} />
-        <Route path="/create"
-          element={user ? <CreateForm user={user} setToggle={setToggle}/>
-            : <Navigate to="/bevs/all" />} />
+        <Route path="/users/:id/create"
+          element={<CreateForm user={user} setToggle={setToggle}/>} />
         <Route path='/sign-up' element={<SignUp user={user} setUser={setUser}/>} />
         <Route path='/sign-in' element={<SignIn user={user} setUser={setUser} />} />
         <Route path='/edit/:id' element={<EditForm user={user} setToggle={setToggle}/>} />
-          <Route path='/bev/:id' element={<BevID user={user} toggle={toggle}/>} />
-        <Route path='/bevs/:typeOf' element={<BevGallery user={user} toggle={toggle} setToggle={setToggle}/>} />
-        <Route path='/about' element={<About/>}/>
+          <Route path='/bev/:bevId' element={<BevID user={user} toggle={toggle} />} />
+          <Route path='/users/:id/bevs/:bevId' element={<BevID user={user} toggle={toggle}/>} />
+          <Route path='/bevs/:typeOf' element={<BevGallery user={user} toggle={toggle} setToggle={setToggle} />} />
+          <Route path='/users/:id/bevs' element={<BevGallery user={user} setToggle={setToggle}/>}/>
+        <Route path='/aboutUs' element={<About/>}/>
       </Routes>
       </Layout>
     
